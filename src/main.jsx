1be386f825ba63bb7976fc94a841a8a213878b6a
Park Ji Hyun <127265212+jihyuncoding@users.noreply.github.com>
2026-09-07 03:21:18 +0900

    Mount live event analytics panel

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import EventAnalyticsPanel from './EventAnalyticsPanel.jsx'
 import './styles.css'
 import './responsive.css'
+import './eventAnalytics.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <EventAnalyticsPanel />
   </React.StrictMode>,
 )
